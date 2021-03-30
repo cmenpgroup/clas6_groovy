6c47c85a2e6da6eb3845404d890a5f332f0594c2
jwgibbs <>
2021-03-30 08:50:37 -0400

    Batch script to run eg2ProtonTree.groovy

---
+++ run_eg2ProtonTree.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -w
+
+$filelist = $ARGV[0];
+
+$jawDir = "$ENV{HOME}/jlab/clas12/jaw-2.1/bin";
+$RunGroovy = "$jawDir/run-groovy";
+
+open(LIST, "$filelist") || die "\n$0: Could not open $filelist.\n";
+while (defined($line=<LIST>)) {
+    chomp($line);
+    $filename = $line;
+    $filename =~ s#.*/##;
+    $outfile = $filename;
+    $outfile =~ s/run/ntuple/;
+    $logfile = $outfile;
+    $logfile =~ s/hipo/log/;
+    $cmd = "$RunGroovy eg2ProtonTree.groovy -o $outfile $line > $logfile";
+    print "$cmd\n";
+    system($cmd);
+}
+close(LIST);
+
