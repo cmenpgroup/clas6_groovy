7f705214f0d773333d28489efb225f1fc41c7b7d
jwgibbs <mikewood@jlab.org>
2021-06-17 09:08:16 -0400

    Added EC fiducial cuts to eg2ProtonTree.groovy

--- eg2ProtonTree.groovy
+++ eg2ProtonTree.groovy
@@ -22,6 +22,11 @@ clas6Proton myProton = new clas6Proton(); // create the proton object
 clas6FidCuts myFidCuts = new clas6FidCuts(); // create the CLAS6 Fiducial Cuts object
 ReactionKine myRK = new ReactionKine(); // create object for reaction kinematics
 
+long st = System.currentTimeMillis(); // start time
+
+int GREEN = 33;
+int BLUE = 34;
+int YELLOW = 35;
 double ELECTRON_MOM = 0.64;
 double NPHE_MIN = 28;
 double ECIN_MIN = 0.06;
@@ -63,6 +68,45 @@ H1F h1_dTOF_not2212 = new H1F("h1_dTOF_not2212",400,-0.5,399.5);
 h1_dTOF_not2212.setTitleX("PID");
 h1_dTOF_not2212.setTitleY("Counts");
 
+int nECx = 450;
+double ECxLo = -450.0;
+double ECxHi = 450.0;
+int nECy = 450;
+double ECyLo = -450.0;
+double ECyHi = 450.0;
+String[] histsECxy = ["h2_EC_XvsY","h2_EC_XvsY_antiFid","h2_EC_XvsY_Fid"];
+H2F[] h2_EC_XvsY = new H2F[histsECxy.size()];
+
+histsECxy.eachWithIndex { hname, ih->
+  h2_EC_XvsY[ih] = new H2F(hname,"EC x vs y",nECx,ECxLo,ECxHi,nECy,ECyLo,ECyHi);
+  h2_EC_XvsY[ih].setTitleX("x (cm)");
+  h2_EC_XvsY[ih].setTitleY("y (cm)");
+}
+
+int nECstrips = 225;
+double ECstripLo = 0.0;
+double ECstripHi = 450.0;
+H1F h1_EC_U = new H1F("h1_EC_U","U (cm)","Counts",nECstrips,ECstripLo,ECstripHi);
+h1_EC_U.setTitle("EC Hit Position");
+
+H1F h1_EC_V = new H1F("h1_EC_V","V (cm)","Counts",nECstrips,ECstripLo,ECstripHi);
+h1_EC_V.setTitle("EC Hit Position");
+
+H1F h1_EC_W = new H1F("h1_EC_W","W (cm)","Counts",nECstrips,ECstripLo,ECstripHi);
+h1_EC_W.setTitle("EC Hit Position");
+
+H1F h1_EC_U_fid = new H1F("h1_EC_U_fid","U (cm)","Counts",nECstrips,ECstripLo,ECstripHi);
+h1_EC_U_fid.setTitle("EC Hit Position");
+h1_EC_U_fid.setFillColor(GREEN);
+
+H1F h1_EC_V_fid = new H1F("h1_EC_V_fid","V (cm)","Counts",nECstrips,ECstripLo,ECstripHi);
+h1_EC_V_fid.setTitle("EC Hit Position");
+h1_EC_V_fid.setFillColor(GREEN);
+
+H1F h1_EC_W_fid = new H1F("h1_EC_W_fid","W (cm)","Counts",nECstrips,ECstripLo,ECstripHi);
+h1_EC_W_fid.setTitle("EC Hit Position");
+h1_EC_W_fid.setFillColor(GREEN);
+
 PhysicsConstants PhyConsts= new PhysicsConstants();
 double LIGHTSPEED = PhyConsts.speedOfLight(); // speed of light in cm/ns
 println "Speed of light = " + LIGHTSPEED + " cm/ns";
@@ -73,9 +117,10 @@ LorentzVector proton = new LorentzVector(0,0,0,0);
 Vector3 v3proton = new Vector3(0,0,0);
 LorentzVector partLV = new LorentzVector(0,0,0,0);
 
-LorentzVector beam = new LorentzVector(0.0,0.0,beamEnergy,beamEnergy);
-LorentzVector protonTarget = new LorentzVector(0.0,0.0,0.0,PhyConsts.massProton());
-myRK.setBeam(beamEnergy,0.0);
+Vector3 ecXYZ = new Vector3(0.0,0.0,0.0);
+Vector3 ecUVW = new Vector3(0.0,0.0,0.0);
+
+myRK.setBeam(beamEnergy,PhyConsts.massElectron());
 myRK.setTarget(PhyConsts.massProton());
 
 def cli = new CliBuilder(usage:'eg2ProtonTree.groovy [options] infile1 infile2 ...')
@@ -84,7 +129,6 @@ cli.M(longOpt:'max',  args:1, argName:'max events' , type: int, 'Filter this num
 cli.c(longOpt:'counter', args:1, argName:'count by events', type: int, 'Event progress counter')
 cli.o(longOpt:'output', args:1, argName:'Ntuple output file', type: String, 'Output file name')
 cli.g(longOpt:'graph', 'Graph monitoring histograms')
-
 
 def options = cli.parse(args);
 if (!options) return;
@@ -124,8 +168,8 @@ Bank       ecpb   = new Bank(reader.getSchemaFactory().getSchema("DETECTOR::ecpb
 Bank       scpb   = new Bank(reader.getSchemaFactory().getSchema("DETECTOR::scpb"));
 
 // Define a ntuple tree with 5 variables
-TreeFileWriter tree = new TreeFileWriter(outFile,"protonTree","Run:Event:iTgt:eNum:eIndex:ePx:ePy:ePz:eTheta:ePhi:eVx:eVy:eVz:pNum:pIndex:pPx:pPy:pPz:pTheta:pPhi:pVx:pVy:pVz:q2:nu:W:zh:zLC:pT2:xb:yb:phiPQ:pFidCut:eFidCut");
-float[]  treeItem = new float[34];
+TreeFileWriter tree = new TreeFileWriter(outFile,"protonTree","Run:Event:iTgt:eNum:eIndex:ePx:ePy:ePz:eTheta:ePhi:eVx:eVy:eVz:pNum:pIndex:pPx:pPy:pPz:pTheta:pPhi:pVx:pVy:pVz:q2:nu:W:zh:zLC:pT2:xb:yb:phiPQ:pFidCut:eFidCut:eFidEC");
+float[]  treeItem = new float[35];
 
 // Loop over all events
 while(reader.hasNext()){
@@ -173,6 +217,7 @@ while(reader.hasNext()){
       boolean cutECoverP = false;
       boolean cutdtECSC = false;
       boolean cutFidCut = false;
+      boolean cutECfid = false;
 
       electron.setPxPyPzM(bank.getFloat("px",val), bank.getFloat("py",val), bank.getFloat("pz",val), PhyConsts.massElectron());
       v3electron.setXYZ(bank.getFloat("vx",val), bank.getFloat("vy",val), bank.getFloat("vz",val));
@@ -191,6 +236,24 @@ while(reader.hasNext()){
         ecout = ecpb.getFloat("eout",bank.getInt("ecstat",val)-1);
         ectot = ecpb.getFloat("etot",bank.getInt("ecstat",val)-1);
         ecTime = ecpb.getFloat("time",bank.getInt("ecstat",val)-1);
+        ecX = ecpb.getFloat("x",bank.getInt("ecstat",val)-1);
+        ecY = ecpb.getFloat("y",bank.getInt("ecstat",val)-1);
+        ecZ = ecpb.getFloat("z",bank.getInt("ecstat",val)-1);
+        ecXYZ.setXYZ(ecX,ecY,ecZ);
+        h2_EC_XvsY[0].fill(ecX,ecY);
+        if(myEC.FidCutXYZ(ecXYZ)){
+          cutECfid = true;
+          h2_EC_XvsY[1].fill(ecX,ecY);
+        } else{
+          h2_EC_XvsY[2].fill(ecX,ecY);
+        }
+        ecUVW = myEC.XYZtoUVW(ecXYZ);
+        h1_EC_U.fill(ecUVW.x());
+        h1_EC_V.fill(ecUVW.y());
+        h1_EC_W.fill(ecUVW.z());
+        if(myEC.FidCutU(ecUVW.x())) h1_EC_U_fid.fill(ecUVW.x());
+        if(myEC.FidCutV(ecUVW.y())) h1_EC_V_fid.fill(ecUVW.y());
+        if(myEC.FidCutW(ecUVW.z())) h1_EC_W_fid.fill(ecUVW.z());
       }
       if(bank.getInt("scstat",val)>0 && scpb.getRows()>0){ // check SC
         cutSCstat = true;
@@ -225,7 +288,7 @@ while(reader.hasNext()){
         cutFidCut = myFidCuts.clas6FidCheckCut(electron,"electron"); // electron fiducial cuts
 
         if(cutQ2 && cutW  && cutYb && cutElectronMom && cutECoverP && cutCCnphe && cutdtECSC && cutECin){
-          ElectronVecList << [electron.px(),electron.py(),electron.pz(),electron.e(),v3electron_corr.x(),v3electron_corr.y(),v3electron_corr.z(),tofElectron,cutFidCut];
+          ElectronVecList << [electron.px(),electron.py(),electron.pz(),electron.e(),v3electron_corr.x(),v3electron_corr.y(),v3electron_corr.z(),tofElectron,cutFidCut,cutECfid];
         }
       }
     }
@@ -287,6 +350,8 @@ while(reader.hasNext()){
       myRK.setScatteredElectron(emVec);
       int emFidCut = 0;         // initialized fiducial cut flag
       if(emList[8]) emFidCut = 1;  // set fiducial cut flag if true
+      int emECFidCut = 0;         // initialized fiducial cut flag
+      if(emList[9]) emECFidCut = 1;  // set fiducial cut flag if true
       ProtonVecList.eachWithIndex { pList, pInd ->
         LorentzVector protonVec = new LorentzVector(pList[0],pList[1],pList[2],pList[3]);
         Vector3 protonVert = new Vector3(pList[4],pList[5],pList[6]);
@@ -329,6 +394,7 @@ while(reader.hasNext()){
           treeItem[31] = Math.toDegrees(myRK.PhiPQ());
           treeItem[32] = pFidCut;
           treeItem[33] = emFidCut;
+          treeItem[34] = emECFidCut;
           tree.addRow(treeItem);  // add the tree data
         }
       }
@@ -362,4 +428,34 @@ if(bGraph){
   c1.cd(3);
   c1.getPad().setTitleFontSize(c1_title_size);
   c1.draw(h1_dTOF_not2212);
+
+  int c2_title_size = 24;
+  TCanvas c2 = new TCanvas("c2",1400,450);
+  c2.divide(3,1);
+  c2.cd(0);
+  c2.getPad().setTitleFontSize(c2_title_size);
+  c2.draw(h1_EC_U);
+  c2.draw(h1_EC_U_fid,"same");
+  c2.cd(1);
+  c2.getPad().setTitleFontSize(c2_title_size);
+  c2.draw(h1_EC_V);
+  c2.draw(h1_EC_V_fid,"same");
+  c2.cd(2);
+  c2.getPad().setTitleFontSize(c2_title_size);
+  c2.draw(h1_EC_W);
+  c2.draw(h1_EC_W_fid,"same");
+
+  int c3_title_size = 24;
+  TCanvas c3 = new TCanvas("c3",1400,450);
+  c3.divide(3,1);
+  histsECxy.eachWithIndex { hname, ih->
+    c3.cd(ih);
+    c3.getPad().setTitleFontSize(c1_title_size);
+    c3.getPad().getAxisZ().setLog(true);
+    c3.draw(h2_EC_XvsY[ih]);
+  }
 }
+
+long et = System.currentTimeMillis(); // end time
+long time = et-st; // time to run the script
+System.out.println(" time = " + (time/1000.0)); // print run time to the screen
