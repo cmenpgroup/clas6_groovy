11508fde4d583a771381076383643240b84dd658
Wood <wood5@canisius.edu>
2022-10-23 21:45:33 -0400

    Perl scripts to run batch jobs

--- run_eg2Proton_MRmulti.pl
+++ run_eg2Proton_MRmulti.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl -w
 
-$IDcut = $ARGV[0];
+$iMR = $ARGV[0];
+$IDcut = $ARGV[1];
 
+@MR = ("MR","MR2pT2","MR3zh");
 @target = ("C","Fe","Pb");
 @tgtDir = ("Carbon","Iron","Lead");
 @cuts =("std","sigma_1_0","sigma_1_5","sigma_2_0","sigma_2_5","sigma_3_0");
@@ -9,24 +11,23 @@ $IDcut = $ARGV[0];
 $jawDir = "$ENV{HOME}/jlab/clas12/jaw-2.1/bin";
 $RunGroovy = "$jawDir/run-groovy";
 
-#for ($i = 0; $i < @target; $i = $i + 1) {
 for $i (0 .. $#target) {
     # create hipo file with uncorrected multiplicity ratios
     $ntupleDir = "$ENV{HOME}/jlab/clas-data/eg2/Npos/$tgtDir[$i]/ntuples_protonID_sysErr/$cuts[$IDcut]";
-    $infile = "$ntupleDir/eg2Proton_jawTree_MR_$target[$i]_Hists_$cuts[$IDcut].hipo";
-    $outfile = "eg2Proton_AnaTree_MR_$target[$i]_Hists_$cuts[$IDcut].hipo";
+    $infile = "$ntupleDir/eg2Proton_jawTree_$MR[$iMR]_$target[$i]_Hists_$cuts[$IDcut].hipo";
+    $outfile = "eg2Proton_AnaTree_$MR[$iMR]_$target[$i]_Hists_$cuts[$IDcut].hipo";
     $logfile = $outfile;
     $logfile =~ s/hipo/log/;
-    $cmd = "$RunGroovy eg2Proton_MR_jawHists.groovy -s $target[$i] -o $outfile $infile > $logfile";
+    $cmd = "$RunGroovy eg2Proton_$MR[$iMR]_jawHists.groovy -s $target[$i] -o $outfile $infile > $logfile";
     print "$cmd\n\n";
     system($cmd);
 
     # create hipo file with corrected multiplicity ratios
     $infile = $outfile;
-    $outfile = "eg2Proton_MRcorr_hists_$target[$i]_$cuts[$IDcut].hipo";
+    $outfile = "eg2Proton_$MR[$iMR]_corr_hists_$target[$i]_$cuts[$IDcut].hipo";
     $logfile = $outfile;
     $logfile =~ s/hipo/log/;
-    $cmd = "$RunGroovy eg2Proton_MR_corr.groovy -s $target[$i] -o $outfile $infile > $logfile";
+    $cmd = "$RunGroovy eg2Proton_$MR[$iMR]_corr.groovy -s $target[$i] -o $outfile $infile > $logfile";
     print "$cmd\n\n";
     system($cmd);
 }
