7faf5e431473dc163969fae84b966037eaaeb02c
Wood <wood5@canisius.edu>
2024-09-06 14:51:25 -0400

    Updated the eg2Proton_MR2pT2_graph2text.groovy

--- eg2Proton_MR2pT2_graph2txt.groovy
+++ eg2Proton_MR2pT2_graph2txt.groovy
@@ -11,33 +11,38 @@ import org.jlab.groot.data.*;
 import org.jlab.groot.ui.*;
 
 OptionParser p = new OptionParser("eg2Proton_MR2pT2_graph2txt.groovy");
+String userTgt = "C";
+p.addOption("-s", userTgt, "Solid Target (C, Fe, Pb)");
+String userSigmaCut = "std";
+p.addOption("-c", userSigmaCut, "Proton ID Cut sigma (std, sigma_1_0, sigma_1_5, sigma_2_0, sigma_2_5, sigma_3_0)");
 p.parse(args);
+userTgt = p.getOption("-s").stringValue();
+userSigmaCut = p.getOption("-c").stringValue();
 
 HistInfo myHI = new HistInfo();
-List<String> solidTgt = myHI.getSolidTgtLabel();
 
 YieldsForMR2pT2 myMR = new YieldsForMR2pT2();
 List<String> zhCuts = myMR.getZhCuts();
 
-TDirectory[] dir = new TDirectory[solidTgt.size()];
-solidTgt.eachWithIndex { nTgt, iTgt ->
-  String fileName = "MR2D/eg2Proton_MR2pT2_corr_hists_" + nTgt + "_std.hipo";
-  println fileName;
-  dir[iTgt] = new TDirectory();
-  dir[iTgt].readFile(fileName);
-}
+String fileName = "MR2pT2/eg2Proton_MR2pT2_corr_hists_" + userTgt + "_" + userSigmaCut + ".hipo";
+println fileName;
+TDirectory dir = new TDirectory();
+dir.readFile(fileName);
 
-GraphErrors[][] gr_mrProtonCorr = new GraphErrors[zhCuts.size()][solidTgt.size()];
-GraphErrors[][] grAcc = new GraphErrors[zhCuts.size()][solidTgt.size()];
+GraphErrors[] gr_mrProtonCorr = new GraphErrors[zhCuts.size()];
+GraphErrors[] gr_mrProton = new GraphErrors[zhCuts.size()];
+GraphErrors[] grAcc = new GraphErrors[zhCuts.size()];
 
 zhCuts.eachWithIndex { nZh, iZh->
   String grcorr = "gr_mrProtonCorr_pT2_" + iZh;
-  String grAccRatio = "gr_acc_" + iZh;
-  solidTgt.eachWithIndex { nTgt, iTgt ->
-    gr_mrProtonCorr[iZh][iTgt] = dir[iTgt].getObject("MR2pT2/",grcorr);
-    gr_mrProtonCorr[iZh][iTgt].save(grcorr + "_" + nTgt + ".txt");
+  gr_mrProtonCorr[iZh] = dir.getObject("MR2pT2/",grcorr);
+  gr_mrProtonCorr[iZh].save(grcorr + "_" + userTgt + "_" + userSigmaCut + ".csv");
 
-    grAcc[iZh][iTgt] = dir[iTgt].getObject("MR2pT2/",grAccRatio);
-    grAcc[iZh][iTgt].save(grAccRatio + "_pT2_" + nTgt + ".txt");
-  }
+  String grUnCorr = "gr_mrProton_pT2_" + iZh;
+  gr_mrProton[iZh] = dir.getObject("MR2pT2/",grUnCorr);
+  gr_mrProton[iZh].save(grUnCorr + "_" + userTgt + "_" + userSigmaCut + ".csv");
+
+  String grAccRatio = "gr_rat" + userTgt + "_" + iZh;
+  grAcc[iZh] = dir.getObject("MR2pT2/",grAccRatio);
+  grAcc[iZh].save(grAccRatio + "_pT2_" + userTgt + "_" + userSigmaCut + ".csv");
 }
