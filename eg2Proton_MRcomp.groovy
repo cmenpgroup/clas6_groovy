9a489e576762ad8d029b10fb5816dbe91dbda499
Wood <wood5@canisius.edu>
2024-09-11 13:53:03 -0400

    WOrking on the Legends in the GraphErrors

--- eg2Proton_MRcomp.groovy
+++ eg2Proton_MRcomp.groovy
@@ -1,31 +1,30 @@
-import org.jlab.jnp.hipo4.data.*;
-import org.jlab.jnp.hipo4.io.*;
-import org.jlab.jnp.physics.*;
-//---- imports for PDG library
-import org.jlab.clas.pdg.PhysicsConstants;
-import org.jlab.clas.pdg.PDGDatabase;
-import org.jlab.clas.pdg.PDGParticle;
+//---- imports for GROOT library
+import org.jlab.groot.data.*;
+import org.jlab.groot.graphics.*;
+import org.jlab.groot.ui.*;
+import org.jlab.groot.base.GStyle;
+import org.jlab.jnp.groot.graphics.TDataCanvas;
+import org.jlab.jnp.groot.graphics.LegendNode2D;
+import org.jlab.jnp.groot.graphics.LegendNode2D.LegendStyle;
+import org.jlab.jnp.groot.settings.GRootColorPalette;
 //---- imports for OPTIONPARSER library
 import org.jlab.jnp.utils.options.OptionParser;
 
-//import org.jlab.jnp.pdg.PhysicsConstants;
-//import org.jlab.jnp.pdg.PDGDatabase;
-//import org.jlab.jnp.pdg.PDGParticle;
-
 import eg2AnaTree.*;
 
-import org.jlab.groot.base.GStyle
-import org.jlab.groot.data.*;
-import org.jlab.groot.ui.*;
-
-GStyle.getAxisAttributesX().setTitleFontSize(18);
-GStyle.getAxisAttributesY().setTitleFontSize(18);
+GStyle.getAxisAttributesX().setTitleFontSize(24);
+GStyle.getAxisAttributesY().setTitleFontSize(24);
 GStyle.getAxisAttributesX().setLabelFontSize(18);
 GStyle.getAxisAttributesY().setLabelFontSize(18);
 GStyle.getAxisAttributesZ().setLabelFontSize(18);
 
 OptionParser p = new OptionParser("eg2Proton_MRcomp.groovy");
+
+String userSigmaCut = "std";
+p.addOption("-c", userSigmaCut, "Proton ID Cut sigma (std, sigma_1_0, sigma_1_5, sigma_2_0, sigma_2_5, sigma_3_0)");
+
 p.parse(args);
+userSigmaCut = p.getOption("-c").stringValue();
 
 HistInfo myHI = new HistInfo();
 List<String> Var = myHI.getVariables();
@@ -33,10 +32,11 @@ List<String> xLabel = myHI.getXlabel();
 List<String> solidTgt = myHI.getSolidTgtLabel();
 
 int c1_title_size = 22;
-TCanvas[] can = new TCanvas[Var.size()];
+TDataCanvas[] can = new TDataCanvas[Var.size()];
+LegendNode2D[] legend = new LegendNode2D[Var.size()];
 TDirectory[] dir = new TDirectory[solidTgt.size()];
 solidTgt.eachWithIndex { nTgt, iTgt ->
-  String fileName = "MR1D/eg2Proton_MR_corr_hists_" + nTgt + "_std.hipo";
+  String fileName = "MR1D/coatjavaHipo/eg2Proton_MR_corr_hists_" + nTgt + "_" + userSigmaCut + "_csv.hipo";
   println fileName;
   dir[iTgt] = new TDirectory();
   dir[iTgt].readFile(fileName);
@@ -45,29 +45,31 @@ solidTgt.eachWithIndex { nTgt, iTgt ->
 GraphErrors[][] gr_mrProtonCorr = new GraphErrors[Var.size()][solidTgt.size()];
 
 Var.eachWithIndex { nVar, iVar->
-  if(nVar!="phiPQ"){
-    String cname = "can" + iVar;
-    can[iVar] = new TCanvas(cname,600,600);
-    can[iVar].cd(0);
-    can[iVar].getPad().setTitleFontSize(c1_title_size);
+  String cname = "can" + iVar;
+  can[iVar] = new TDataCanvas(600,600);
+  can[iVar].cd(0);
+//  can[iVar].getPad().setTitleFontSize(c1_title_size);
 //    if(nVar=="zh") can[iVar].getPad().getAxisY().setLog(true);
 
-    solidTgt.eachWithIndex { nTgt, iTgt ->
-      String grcorr = "gr_mrProtonCorr_" + nVar;
-      gr_mrProtonCorr[iVar][iTgt] = dir[iTgt].getObject(nVar,grcorr);
-      gr_mrProtonCorr[iVar][iTgt].setTitleX(xLabel[iVar]);
-      gr_mrProtonCorr[iVar][iTgt].setTitleY("R^p");
-      gr_mrProtonCorr[iVar][iTgt].setMarkerColor(iTgt+1);
-      gr_mrProtonCorr[iVar][iTgt].setLineColor(iTgt+1);
-      gr_mrProtonCorr[iVar][iTgt].setMarkerSize(5);
-      gr_mrProtonCorr[iVar][iTgt].setMarkerStyle(iTgt);
-      if(iTgt==0){
-        can[iVar].draw(gr_mrProtonCorr[iVar][iTgt]);
-      }else{
-        can[iVar].draw(gr_mrProtonCorr[iVar][iTgt],"same");
-      }
-    }
+  legend[iVar] = new LegendNode2D(400,30);
+
+  String imgRp = "eg2Proton_MR_comp_" + nVar + "_" + userSigmaCut + ".png";
+
+  solidTgt.eachWithIndex { nTgt, iTgt ->
+    String grcorr = "gr_mrProtonCorr_" + nVar;
+    gr_mrProtonCorr[iVar][iTgt] = dir[iTgt].getObject(nVar,grcorr);
+    gr_mrProtonCorr[iVar][iTgt].setTitleX(xLabel[iVar]);
+    gr_mrProtonCorr[iVar][iTgt].setTitleY("R_p");
+    gr_mrProtonCorr[iVar][iTgt].setMarkerColor(iTgt+1);
+    gr_mrProtonCorr[iVar][iTgt].setLineColor(iTgt+1);
+    gr_mrProtonCorr[iVar][iTgt].setMarkerSize(5);
+    gr_mrProtonCorr[iVar][iTgt].setMarkerStyle(iTgt);
+    legend[iVar].add(gr_mrProtonCorr[iVar][iTgt],nTgt);
+    can[iVar].draw(gr_mrProtonCorr[iVar][iTgt],"same");
   }
+  can[iVar].getDataCanvas().getRegion(0).addNode(legend[iVar]);
+  can[iVar].repaint();
+//  can[iVar].save(imgRp);
 }
 
 TCanvas[] canAcc = new TCanvas[Var.size()];
@@ -75,26 +77,27 @@ GraphErrors[][] grAcc = new GraphErrors[Var.size()][solidTgt.size()];
 
 // loop over the 1-D variables
 Var.eachWithIndex { nVar, iVar->
-  if(nVar!="phiPQ"){
-    String cname = "canAcc" + iVar;
-    canAcc[iVar] = new TCanvas(cname,600,600);
-    canAcc[iVar].cd(0);
-    canAcc[iVar].getPad().setTitleFontSize(c1_title_size);
+  String cname = "canAcc" + iVar;
+  canAcc[iVar] = new TCanvas(cname,600,600);
+  canAcc[iVar].cd(0);
+  canAcc[iVar].getPad().setTitleFontSize(c1_title_size);
 
-    solidTgt.eachWithIndex { nTgt, iTgt ->
-      String grcorr = "gr_acc_" + nVar;
-      grAcc[iVar][iTgt] = dir[iTgt].getObject(nVar,grcorr);
-      grAcc[iVar][iTgt].setTitleX(xLabel[iVar]);
-      grAcc[iVar][iTgt].setTitleY("Acceptance Ratio");
-      grAcc[iVar][iTgt].setMarkerColor(iTgt+1);
-      grAcc[iVar][iTgt].setLineColor(iTgt+1);
-      grAcc[iVar][iTgt].setMarkerSize(5);
-      grAcc[iVar][iTgt].setMarkerStyle(iTgt);
-      if(iTgt==0){
-        canAcc[iVar].draw(grAcc[iVar][iTgt]);
-      }else{
-        canAcc[iVar].draw(grAcc[iVar][iTgt],"same");
-      }
+  String imgAccRat = "eg2Proton_MR_comp_" + nVar + "_" + userSigmaCut + "_AccRat.png";
+
+  solidTgt.eachWithIndex { nTgt, iTgt ->
+    String grcorr = "gr_rat" + nTgt + "_" + nVar;
+    grAcc[iVar][iTgt] = dir[iTgt].getObject(nVar,grcorr);
+    grAcc[iVar][iTgt].setTitleX(xLabel[iVar]);
+    grAcc[iVar][iTgt].setTitleY("Acceptance Ratio");
+    grAcc[iVar][iTgt].setMarkerColor(iTgt+1);
+    grAcc[iVar][iTgt].setLineColor(iTgt+1);
+    grAcc[iVar][iTgt].setMarkerSize(5);
+    grAcc[iVar][iTgt].setMarkerStyle(iTgt);
+    if(iTgt==0){
+      canAcc[iVar].draw(grAcc[iVar][iTgt]);
+    }else{
+      canAcc[iVar].draw(grAcc[iVar][iTgt],"same");
     }
   }
+  canAcc[iVar].save(imgAccRat);
 }
